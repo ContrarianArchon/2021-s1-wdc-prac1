7c95fb01ffb024dcdc49d19dd9885e4d5a66d057
ContrarianArchon <83257419+ContrarianArchon@users.noreply.github.com>
2022-03-30 05:07:44 +0000

    task 3-2 complete

--- prac3/task3.js
+++ prac3/task3.js
@@ -11,8 +11,9 @@ element.addEventListener("click", makePost);
 function makePost(){
 //make paragraph, class "post-date" with the current date in it
 var d = new Date();
-document.getElementById("posts").innerHTML += "<p class='post-date'>" + date + "</p>"
+document.getElementById("posts").innerHTML += "<p class='post-time'>" + d + "</p>";
 //extract text from the posting box
-
+var text = document.getElementById("TextInput").value;
 //make paragraph, class post-content, with the extracted text in it.
+document.getElementById("posts").innerHTML += "<p class='post-content'>" + text + "</p>";
 }

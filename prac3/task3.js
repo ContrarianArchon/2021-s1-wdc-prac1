764a147acc49a1a699b1cd926cf5ee2302cf16a0
ContrarianArchon <83257419+ContrarianArchon@users.noreply.github.com>
2022-03-30 04:56:22 +0000

    Wed, Mar 30, 2022, 4:56 AM +00:00

--- prac3/task3.js
+++ prac3/task3.js
@@ -3,3 +3,16 @@ function CountIncrement(x){
     count++;
     x.innerHTML = count;
 }
+
+//on event: clicking the post button
+const element = document.getElementById("PostBtn");
+element.addEventListener("click", makePost);
+
+function makePost(){
+//make paragraph, class "post-date" with the current date in it
+var d = new Date();
+document.getElementById("posts").innerHTML += "<p class='post-date'>" + date + "</p>"
+//extract text from the posting box
+
+//make paragraph, class post-content, with the extracted text in it.
+}

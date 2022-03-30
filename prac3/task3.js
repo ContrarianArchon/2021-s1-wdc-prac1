0d905a8af3831d590b582a02ff4f550c8f2b64e3
ContrarianArchon <83257419+ContrarianArchon@users.noreply.github.com>
2022-03-30 07:48:28 +0000

    task 3-4 complete

--- prac3/task3.js
+++ prac3/task3.js
@@ -13,13 +13,26 @@ const BackBtn = document.getElementById("BackBtn");
 BackBtn.addEventListener("click", hideMenu);
 
 function makePost(){
-//make paragraph, class "post-date" with the current date in it
-var d = new Date();
-document.getElementById("posts").innerHTML += "<p class='post-time'>" + d + "</p>";
-//extract text from the posting box
-var text = document.getElementById("TextInput").value;
-//make paragraph, class post-content, with the extracted text in it.
-document.getElementById("posts").innerHTML += "<p class='post-content'>" + text + "</p>";
+    //make paragraph, class "post-date" with the current date in it
+    var d = new Date();
+    //extract text from the posting box
+    var text = document.getElementById("TextInput").value;
+    //extract desired text-color from the form.
+    var postColor = "black";
+    if(document.getElementById("blue").checked){
+        postColor = "blue";
+    }
+    if(document.getElementById("red").checked){
+        postColor = "red";
+    }
+    //extract desired number of posts from the form.
+    var postCount = document.getElementById("quantity").value;
+    //loop for i < postCount.
+    //make paragraph, class post-content, with the extracted text in it.
+    for(let i = 0; i < postCount; i++){
+        document.getElementById("posts").innerHTML += "<p class='post-time'>" + d + "</p>";
+        document.getElementById("posts").innerHTML += "<p class='post-content' style='color:" + postColor +"'>" + text + "</p>";
+    }
 }
 
 function showMenu(){

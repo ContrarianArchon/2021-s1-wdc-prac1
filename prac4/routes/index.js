7154f6109ff587141ebc1ace46624f32c6b7b6bf
ContrarianArchon <83257419+ContrarianArchon@users.noreply.github.com>
2022-05-23 12:10:17 +0000

    Mon, May 23, 2022, 12:10 PM +00:00

--- prac4/routes/index.js
+++ prac4/routes/index.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+var lastmessage;
+router.get('/test.txt', function(req, res, next){
+  res.send(lastmessage.toString());
+  lastmessage = new date();
+});
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });

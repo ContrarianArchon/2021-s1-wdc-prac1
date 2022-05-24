8152679e51cf23f3aa56c83d3dd953c9ef87840b
ContrarianArchon <83257419+ContrarianArchon@users.noreply.github.com>
2022-05-24 12:33:30 +0000

    2.3 done

--- prac7/routes/users.js
+++ prac7/routes/users.js
@@ -4,6 +4,15 @@ router.post('/*', function(req, res, next){
   console.log("POST from a user");
   next();
 })
+router.post('/*', function(req, res, next){
+  if(req.get('content-type') == undefined){
+    res.status(418);
+    res.send();
+  }else{
+    next();
+  }
+})
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');

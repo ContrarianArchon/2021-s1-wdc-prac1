fb7f96f2a481cecc0139871f4272b9614a5ee988
ContrarianArchon <83257419+ContrarianArchon@users.noreply.github.com>
2022-05-24 12:27:37 +0000

    2.2 done

--- prac7/routes/users.js
+++ prac7/routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-router.post('/', function(req, res, next){
+router.post('/*', function(req, res, next){
   console.log("POST from a user");
   next();
 })

102bc2928274c4d1428b08d85a92e527560aadf2
ContrarianArchon <83257419+ContrarianArchon@users.noreply.github.com>
2022-05-24 10:55:32 +0000

    prac 7 better late than never, started

--- prac7/routes/index.js
+++ prac7/routes/index.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+/*1.1 brew handling.*/
+router.get('/brew', function(req, res, next){
+  if(req.query.drink == "tea"){
+    res.send("A delicious cup of tea!");
+  }else if(req.query.drink == "coffee"){
+    res.status(418);
+    res.send();
+  }else{
+    res.status(400);
+    res.send();
+  }
+});
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });

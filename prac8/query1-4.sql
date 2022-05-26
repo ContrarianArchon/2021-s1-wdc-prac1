6a37f435e0a87483a3ce355ab42ebec59865a5c9
ContrarianArchon <83257419+ContrarianArchon@users.noreply.github.com>
2022-05-26 14:35:33 +0000

    task 2.4 complete

--- prac8/query1-4.sql
+++ prac8/query1-4.sql
@@ -1,1 +1,5 @@
-SELECT * FROM store, [the table tracking stock], [the table tracking film-ID correspondance], where [stock is greater than zero];
+SELECT DISTINCT address.address FROM address
+INNER JOIN store on store.address_id = address.address_id
+INNER JOIN inventory ON store.store_id = inventory.store_id
+INNER JOIN film ON inventory.film_id = film.film_id
+WHERE film.title = "TWISTED PIRATES";

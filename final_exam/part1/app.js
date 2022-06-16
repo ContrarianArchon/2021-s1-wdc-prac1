90f460d26a9d738b5edac9775296eca44a289dbc
ContrarianArchon <83257419+ContrarianArchon@users.noreply.github.com>
2022-06-16 08:53:03 +0000

    Thu, Jun 16, 2022, 8:53 AM +00:00

--- final_exam/part1/app.js
+++ final_exam/part1/app.js
@@ -2,13 +2,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var searchRouter = require('./routes/search');
 
+var dbConnectionPool = mysql.createPool({
+    host: 'localhost',
+    database: 'hotel'
+  });
+
 var app = express();
 
+app.use(function(req, res, next) {
+  req.pool = dbConnectionPool;
+  next();
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

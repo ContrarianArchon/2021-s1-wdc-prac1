fd08e0e0ec11e2cbca500f4b5a6428bd83b198de
ContrarianArchon <83257419+ContrarianArchon@users.noreply.github.com>
2022-06-16 17:48:46 +0000

    Thu, Jun 16, 2022, 5:48 PM +00:00

--- final_exam/part2/routes/index.js
+++ final_exam/part2/routes/index.js
@@ -39,7 +39,7 @@ router.get('/posts', function(req, res, next) {
 
 router.post('/posts/new', function(req, res, next) {
 
-    if(!'user' in req.session){
+    if(!('user' in req.session)){
       res.sendStatus(401);
       return;
     }
@@ -109,7 +109,7 @@ router.get('/posts/:post_id/comments', function(req, res, next) {
 
 router.post('/posts/:post_id/delete', function(req, res, next) {
 
-  if(!'user' in req.session){
+  if(!('user' in req.session)){
     res.sendStatus(401);
     return;
   }
@@ -146,7 +146,7 @@ router.post('/posts/:post_id/delete', function(req, res, next) {
 
 router.post('/comments/new', function(req, res, next) {
 
-  if(!'user' in req.session){
+  if(!('user' in req.session)){
     res.sendStatus(401);
     return;
   }
